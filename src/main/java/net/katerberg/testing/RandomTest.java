20ef94910550acab7653ebf5553f89b43c9c47fe
diablomarcus <diablomarcus@gmail.com>
2013-02-18 10:59:14 -0600

    Made jar a bit more robust

--- src/main/java/net/katerberg/testing/RandomTest.java
+++ src/main/java/net/katerberg/testing/RandomTest.java
@@ -5,24 +5,36 @@ import java.util.Random;
 public class RandomTest {
 
 	public static Random random = new Random();
-	
+
 	public static String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	
+
 	public static char getRandomCharacter() {
-		Character character = alpha.charAt(random.nextInt(alpha.length()));
-		
-		return random.nextBoolean() ? character : Character.toLowerCase(character);
+		char upperCharacter = getRandomCharacter();
+		return random.nextBoolean() ? upperCharacter : Character
+				.toLowerCase(upperCharacter);
 	}
-	
+
+	public static char getRandomUpperCharacter() {
+		return alpha.charAt(random.nextInt(alpha.length()));
+	}
+
+	public static char getRandomLowerCharacter() {
+		return Character.toLowerCase(getRandomCharacter());
+	}
+
 	public static String getRandomString(Integer length) {
 		StringBuilder result = new StringBuilder();
-		for (int i=0; i<length; i++){
+		for (int i = 0; i < length; i++) {
 			result.append(getRandomCharacter());
 		}
 		return result.toString();
 	}
-	
+
 	public static String getRandomString() {
-		return getRandomString(random.nextInt(20)+1);
+		return getRandomString(random.nextInt(20) + 1);
+	}
+	
+	public static int getRandomNumber() {
+		return random.nextInt();
 	}
 }
